99f76712b0d76e0334d6ba73d1f471c4a055cdf4
kantainab <kantainab@gmail.com>
2022-09-08 16:58:00 -0700

    anotherone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
-
-/**
+/*
  *main - this program prints "Programming is like building a multilingual puzzle
  *
  * Return: 0
  *
  */
-
 int main(void)
-
  /*main - this program prints "Programming is like building a multilingual puzzle" */
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-
 /**
- *
  * main - print the string in the put function
  *
  * Description: using the main function
@@ -9,15 +7,9 @@
  * Return: 0
  *
  */
-
 int main(void)
-
 /* this program prints "with proper grammer, but the out come is a piece of art */
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-
 			return (0);
-
 }
