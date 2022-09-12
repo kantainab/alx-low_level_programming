618ebe43d727316a2f236bef4d6e885e5038fadf
kantainab <kantainab@gmail.com>
2022-09-12 12:55:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 	/**
-	 * main - using the function puts
+	 * main - Entry point
 	 * Return: Always 0 (success)
 	 */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
