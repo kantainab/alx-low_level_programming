513de5fe2a876d975f4cba4eb0737dbc2ebabecc
kantainab <kantainab@gmail.com>
2022-09-12 13:09:19 -0700

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
-#include <stdio.h> 
+#include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -6,5 +7,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
