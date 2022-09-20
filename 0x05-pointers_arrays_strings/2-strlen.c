fe157078256a2b5f097f6e8c888ccf62c5ef7279
kantainab <kantainab@gmail.com>
2022-09-20 16:33:02 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: this is the string to check
+ * Return: int length of the string
+ * in this case sl is var name for the string length
+ */
+
+int _strlen(char *s)
+{
+	int sl;
+
+	for (sl = 0; s[sl] != '\0'; sl++)
+	{}
+	return (sl);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ *  _puts - print string with new line
+ *  @str: this is the pointer for the string to be printed
+ *  Return always 0.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar("\n")
+}
+			i++;
+	}
+	_putchar("\n");
+}
